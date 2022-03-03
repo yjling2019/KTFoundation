1ac9202ac1d7d126d1b67bcd018aa801c88c041e
KOTU <yjling2019@gmail.com>
2022-03-03 11:17:19 +0800

    Initial commit

---
+++ Example/KTFoundation/KTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KTAppDelegate.h
+//  KTFoundation
+//
+//  Created by KOTU on 03/03/2022.
+//  Copyright (c) 2022 KOTU. All rights reserved.
+//
+
+@import UIKit;
+
+@interface KTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/KTFoundation/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  KTFoundation
+//
+//  Created by KOTU on 03/03/2022.
+//  Copyright (c) 2022 KOTU. All rights reserved.
+//
+
+@import UIKit;
+#import "KTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KTAppDelegate class]));
+    }
+}
